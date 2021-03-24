76304a053304871772f31df4f5643b362683aaea
luke@jongde.com <luke@jongde.com>
2021-03-24 13:58:12 +0800

    add warn error message

--- src/components/Redux/Message/index.tsx
+++ src/components/Redux/Message/index.tsx
@@ -8,17 +8,18 @@ const Message: FC = () => {
   // the component will render by Deconstructing object
   // const { type } = useSelector((state: IRootState) => state.message);
   const type = useSelector((state: IRootState) => state.message.type);
+  const message = useSelector((state: IRootState) => state.message.message);
 
   const dispatch = useDispatch();
   const messageDispatcher = getMessageDispatcher(dispatch);
 
-  console.log(type, typeof type);
+  console.log(type, message);
 
   return (
     <div className="c-redux-message">
-      <button onClick={() => messageDispatcher.info('message test')}>info</button>
-      <button>warn</button>
-      <button>error</button>
+      <button onClick={() => messageDispatcher.info('this is the information')}>info</button>
+      <button onClick={() => messageDispatcher.warn('this is the warning')}>warn</button>
+      <button onClick={() => messageDispatcher.error('this is the error')}>error</button>
       <button onClick={() => messageDispatcher.dismiss()}>dismiss</button>
     </div>
   );
--- src/redux/dispatchers/message.ts
+++ src/redux/dispatchers/message.ts
@@ -4,6 +4,8 @@ import { MessageActions } from '../actions';
 
 export interface IMessageDispatcher {
   info(message: string): void;
+  warn(message: string): void;
+  error(message: string): void;
   dismiss(): void;
 }
 
@@ -11,6 +13,12 @@ export const getMessageDispatcher = (dispatch: Dispatch): IMessageDispatcher => 
   return {
     info: (message: string) => {
       dispatch(MessageActions.info(message));
+    },
+    warn: (message: string) => {
+      dispatch(MessageActions.warn(message));
+    },
+    error: (message: string) => {
+      dispatch(MessageActions.error(message));
     },
     dismiss: () => {
       dispatch(MessageActions.dismiss());
